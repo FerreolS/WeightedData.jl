2e724f8bbaccb05db1b1be368f16c92301c1d4fb
ferreol <ferreol.soulez@univ-lyon1.fr>
2025-03-25 18:13:24 +0100

    first use of Runic

--- src/WeightedData.jl
+++ src/WeightedData.jl
@@ -1,7 +1,7 @@
 module WeightedData
 using ChainRulesCore
 
-export WeightedPoint,combine,likelihood,scaledlikelihood,gausslikelihood,robustlikelihood,flagbadpix
+export WeightedPoint, combine, likelihood, scaledlikelihood, gausslikelihood, robustlikelihood, flagbadpix
 
 include("WeightedPoint.jl")
 include("arrays.jl")
--- src/arrays.jl
+++ src/arrays.jl
@@ -1,11 +1,10 @@
-
 ## Array of WeightedPoint
 
-get_val(x::AbstractArray{WeightedPoint{T},N}) where {T,N} = map(x->x.val, x)
-get_precision(x::AbstractArray{WeightedPoint{T},N}) where {T,N} = map(x->x.precision, x)
+get_val(x::AbstractArray{WeightedPoint{T}, N}) where {T, N} = map(x -> x.val, x)
+get_precision(x::AbstractArray{WeightedPoint{T}, N}) where {T, N} = map(x -> x.precision, x)
 
-function flagbadpix(data::AbstractArray{WeightedPoint{T},N},badpix::Union{ Array{Bool, N},BitArray{N}}) where {T,N}
+function flagbadpix(data::AbstractArray{WeightedPoint{T}, N}, badpix::Union{Array{Bool, N}, BitArray{N}}) where {T, N}
     size(data) == size(badpix) || error("flagbadpix! : size(A) != size(badpix)")
-	map((d,flag) -> ifelse(flag,WeightedPoint(T(0),T(0)),d ),data,badpix) 
-	
+    return map((d, flag) -> ifelse(flag, WeightedPoint(T(0), T(0)), d), data, badpix)
+
 end
