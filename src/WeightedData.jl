a58cb2eff20066d4ec47daa057ddf4fff9202ea3
ferreol <ferreol.soulez@univ-lyon1.fr>
2026-02-26 10:36:51 +0100

    make weightedmean, ScaledL2Loss,flagbaddata,flagbaddata! rather than exported

--- src/WeightedData.jl
+++ src/WeightedData.jl
@@ -5,15 +5,27 @@ import TypeUtils
 
 
 export WeightedValue,
-    weightedmean,
     likelihood,
-    flagbadpix,
-    flagbadpix!,
     WeightedArray,
     get_value,
     get_precision,
-    get_weight,
-    ScaledL2Loss
+    get_weight
+
+if VERSION >= v"1.11"
+    Core.eval(
+        @__MODULE__, Expr(
+            :public,
+            :weightedmean,
+            :ScaledL2Loss,
+            :flagbaddata,
+            :flagbaddata!,
+        )
+    )
+else
+    @eval begin
+        export weightedmean, ScaledL2Loss, flagbaddata, flagbaddata!
+    end
+end
 
 include("WeightedValue.jl")
 include("WeightedArray.jl")
