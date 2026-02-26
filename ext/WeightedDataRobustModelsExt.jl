62ebb09272ad856644666ae618f08ffe1a63d636
ferreol <ferreol.soulez@univ-lyon1.fr>
2026-02-26 16:22:49 +0100

    Add workingweights function and update imports in WeightedDataRobustModelsExt.jl

--- ext/WeightedDataRobustModelsExt.jl
+++ ext/WeightedDataRobustModelsExt.jl
@@ -24,7 +24,8 @@ import RobustModels: LossFunction,
     HampelLoss,
     loss,
     rho,
-    weight
+    weight,
+    workingweights
 
 
 """
@@ -74,8 +75,31 @@ function get_weight(loss::LossFunction, data::AbstractArray{<:WeightedValue}, mo
     size(data) == size(model) || error("likelihood : size(A) != size(model)")
     r = @. sqrt($get_precision(data)) * (model - $get_value(data))
     w = Base.Fix1(weight, loss)
-    map(w, r)
+    return map(w, r)
 end
+
+"""
+    workingweights(loss::LossFunction, data::AbstractArray{<:WeightedValue}, model::AbstractArray)
+
+Compute the working weights for a given loss function, weighted data, and model.
+
+This function calculates the weights used in iterative weighted least squares algorithms
+by delegating to [`get_weight`](@ref) with the specified loss function, data, and model parameters.
+
+# Arguments
+- `loss::LossFunction`: The loss function defining the weighting scheme
+- `data::AbstractArray{<:WeightedValue}`: Array of weighted data points
+- `model::AbstractArray`: Model parameters or predictions
+
+# Returns
+Array of working weights corresponding to the input data
+
+# See Also
+- [`get_weight`](@ref)
+- [`LossFunction`](@ref)
+- [`WeightedValue`](@ref)
+"""
+workingweights(loss::LossFunction, data::AbstractArray{<:WeightedValue}, model::AbstractArray) = get_weight(loss, data, model)
 
 end
 
