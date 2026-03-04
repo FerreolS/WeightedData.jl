c4054ac854b7135743e2946ce875aed0ac6f1536
ferreol <ferreol.soulez@univ-lyon1.fr>
2026-03-04 16:00:08 +0100

    update adapt_structure to use _WeightedArray for prevent the allocation of new array test: add additional tests to ensure that adapt output the right backend

--- ext/WeightedDataAdaptExt.jl
+++ ext/WeightedDataAdaptExt.jl
@@ -11,6 +11,6 @@ Adapt a `WeightedArray` structure to a target backend using `Adapt.jl`.
 Both value and precision arrays are adapted consistently and wrapped back into
 `WeightedArray`.
 """
-adapt_structure(to, wd::WeightedArray) = 
-	WeightedArray(adapt(to, get_value(wd)),adapt(to, get_precision(wd)))
+adapt_structure(to, wd::WeightedArray) =
+    _WeightedArray(adapt(to, get_value(wd)), adapt(to, get_precision(wd)))
 end
--- test/WeightedDataAdaptExt_test.jl
+++ test/WeightedDataAdaptExt_test.jl
@@ -5,6 +5,9 @@ import WeightedData: get_value, get_precision
 @testset "WeightedDataAdaptExt" begin
     data = WeightedArray(Float32[1.0, 2.0, 3.0], Float32[0.5, 0.2, 1.5])
 
+    @test get_value(data) isa Vector{Float32}
+    @test get_precision(data) isa Vector{Float32}
+
     data_gpu_like = adapt(JLArray, data)
     @test get_value(data_gpu_like) isa JLArray
     @test get_precision(data_gpu_like) isa JLArray
@@ -14,4 +17,13 @@ import WeightedData: get_value, get_precision
     data_back = adapt(Array, data_gpu_like)
     @test get_value(data_back) == get_value(data)
     @test get_precision(data_back) == get_precision(data)
+
+    # Adapting to an already matching backend should preserve backend/storage kind.
+    data_gpu_like_2 = adapt(JLArray, data_gpu_like)
+    @test get_value(data_gpu_like_2) isa JLArray
+    @test get_precision(data_gpu_like_2) isa JLArray
+
+    data_cpu_2 = adapt(Array, data_back)
+    @test get_value(data_cpu_2) isa Vector{Float32}
+    @test get_precision(data_cpu_2) isa Vector{Float32}
 end
